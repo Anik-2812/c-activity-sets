4cde6f655ce062b22412b400f30dcc680d24b0d3
Your Name <anik41230@gmail.com>
2026-09-19 17:38:02 +0530

    Add program to print name

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,1 +1,6 @@
 // 1. Write a C program to print your name.
+#include <stdio.h>
+int main(){
+    printf("Name: Anik Sarkar\n");
+    return 0;
+}
--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,21 @@
 //2. Write a C program to add two numbers.
-
+#include<stdio.h>
+void input(int *a, int *b)
+{
+    printf("INPUT:\na= ");
+    scanf("%d",a);
+    printf("b= ");
+    scanf("%d",b);
+}
+int add(int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    int a,b,sum;
+    input(&a,&b);
+    sum=add(a,b);
+    printf("OUTPUT:\n%d + %d = %d\n",a,b,sum);
+    return 0;
+}
